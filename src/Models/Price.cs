47f83a4a9f6a49d10ac081e3b1b8df6998122a59
Olof Enström <oe222fh@student.lnu.se>
2023-04-22 15:01:59 +0200

    Add Price model

---
+++ src/Models/Price.cs
@@ -0,0 +1,13 @@
+﻿namespace src.Models;
+
+public class Price {
+  public int PriceValueId { get; set; }
+  public DateTime Created { get; set; }
+  public DateTime Modified { get; set; }
+  public string? CatalogEntryCode { get; set; }
+  public string? MarketId { get; set; }
+  public string? CurrencyCode { get; set; }
+  public DateTime ValidFrom { get; set; }
+  public DateTime? ValidUntil { get; set; }
+  public decimal UnitPrice { get; set; }
+}
