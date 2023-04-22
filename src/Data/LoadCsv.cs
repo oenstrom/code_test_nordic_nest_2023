e4e88a41d314213409bdf0a1f2f5d74077fcc8f9
Olof Enström <oe222fh@student.lnu.se>
2023-04-22 17:35:09 +0200

    Add class for loading the CSV data into the db if it does not exist

---
+++ src/Data/LoadCsv.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using CsvHelper;
+using src.Models;
+
+namespace src.Data;
+
+public class LoadCsv {
+  private readonly ApplicationDbContext _context;
+
+  public LoadCsv(ApplicationDbContext context) {
+    _context = context;
+  }
+
+  /**
+   * Load the CSV file into the database if it doesn't exist.
+   */
+  public void Load() {
+    const string csvPath = @"price_detail.csv";
+
+    using var reader = new StreamReader(csvPath);
+    using var csv = new CsvReader(
+      reader,
+      new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = "\t" }
+    );
+    var records = csv.GetRecords<Price>();
+    if (!_context.Database.EnsureCreated()) return;
+    _context.Price.AddRange(records);
+    _context.SaveChanges();
+  }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using src.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("ApplicationDbContext") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.")));
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<LoadCsv>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope()) {
+    var loadCsv = scope.ServiceProvider.GetService<LoadCsv>();
+    loadCsv?.Load();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
