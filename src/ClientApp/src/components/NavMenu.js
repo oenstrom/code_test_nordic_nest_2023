f1d51c9b83bb8a133331a4f3c48485a726ab2df3
Olof Enström <oe222fh@student.lnu.se>
2023-04-22 12:55:05 +0200

    Replace bootstrap with Tailwind

--- src/ClientApp/src/components/NavMenu.js
+++ src/ClientApp/src/components/NavMenu.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import './NavMenu.css';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -23,24 +21,15 @@ export class NavMenu extends Component {
 
   render() {
     return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">src</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-              </NavItem>
-            </ul>
-          </Collapse>
-        </Navbar>
+      <header className="flex">
+        <Link to="/">Optimized Price</Link>
+        <nav className="flex">
+          <ul className="navbar-nav flex-grow">
+            <Link className="text-dark" to="/">Home</Link>
+            <Link className="text-dark" to="/counter">Counter</Link>
+            <Link className="text-dark" to="/fetch-data">Fetch data</Link>
+          </ul>
+        </nav>
       </header>
     );
   }
