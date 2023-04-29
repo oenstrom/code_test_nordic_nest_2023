3402a5189e26d2e7def192097a7f2ac405b3e8de
Olof Enström <oe222fh@student.lnu.se>
2023-04-29 14:56:02 +0200

    Finished the algorithm. Should cover most cases.

--- testing.py
+++ testing.py
@@ -1,18 +1,19 @@
 ﻿data = [
-    # {
-    #     "validFrom": "1970-01-01T00:00:00",
-    #     "validUntil": None,
-    #     "unitPrice": 439.6
-    # },
+    {
+        "validFrom": "1970-01-01T00:00:00",
+        "validUntil": None,
+        "unitPrice": 439.6
+    },
     {
         "validFrom": "2018-06-18T00:00:00",
         "validUntil": "2018-08-05T00:00:00",
+        # "validUntil": "2018-08-27T00:00:00",
         "unitPrice": 399.6
+        # "unitPrice": 599.6
     },
     {
         "validFrom": "2018-08-01T00:00:00",
-        # "validUntil": "2018-08-05T00:00:00",
-        "validUntil": "2018-08-03T00:00:00",
+        "validUntil": "2018-08-05T00:00:00",
         "unitPrice": 326.8
     },
     {
@@ -55,14 +56,19 @@ expected_output = [
     },
 ]
 
-optimized_price = []
 
-# Sort on validFrom, get all entries with validUntil = None, add them to a list
-null_prices = [d for d in sorted(data, key=lambda x: x["validFrom"]) if d["validUntil"] is None]
+# Sort on  unitPrice low to high then on validUntil highest to lowest
+lowest_price_data = sorted(data, key=lambda x: (-x["unitPrice"], x["validUntil"]), reverse=True)
 
 # Sort the data on validFrom, validUntil, unitPrice
 sorted_data = sorted(data, key=lambda x: (x["validFrom"], x["validUntil"] if x["validUntil"] else "9999-12-31T00:00:00", -x["unitPrice"]))
+# Set all None values to 9999-12-31T00:00:00
+for d in sorted_data:
+    if d["validUntil"] is None:
+        d["validUntil"] = "9999-12-31T00:00:00"
 
+
+optimized_price = []
 # Add the first entry from sorted_data to optimized_price
 optimized_price.append({
     "validFrom": sorted_data[0]["validFrom"],
@@ -70,56 +76,45 @@ optimized_price.append({
     "unitPrice": sorted_data[0]["unitPrice"]
 })
 
-# Loop through the sorted data
-for i, entry in enumerate(sorted_data):
-    if i == 0:
+done = False
+previous = sorted_data.pop(0)
+while sorted_data:
+    current = sorted_data.pop(0)
+
+    # If there is a gap between the current price and the previous price
+    if current["validFrom"] > previous["validUntil"]:
+        # Add the lowest price that fits in the gap
+        for entry in lowest_price_data:
+            if entry["validFrom"] <= previous["validUntil"] and entry["validUntil"] > previous["validUntil"]:
+                temp = { "validFrom": previous["validUntil"], "validUntil": entry["validUntil"], "unitPrice": entry["unitPrice"] }
+                optimized_price.append(temp)
+                previous = temp
+                sorted_data.insert(0, current)
+                break
+        # No price fits in the gap, add the current price to the optimized_price
+        else:
+            optimized_price.append({ "validFrom": current["validFrom"], "validUntil": current["validUntil"], "unitPrice": current["unitPrice"] })
+            previous = current
         continue
+
     
-    # Om föregående validUntil är None eller större än nuvarande validFrom. Sätt in nuvarande validFrom som validUntil och lägg till nuvarande entry
-    if optimized_price[i - 1]["validUntil"] is None or entry["validFrom"] <= optimized_price[i - 1]["validUntil"]:
-        optimized_price[i - 1]["validUntil"] = entry["validFrom"]
-        optimized_price.append({
-            "validFrom": entry["validFrom"],
-            "validUntil": entry["validUntil"],
-            "unitPrice": entry["unitPrice"]
-        })
-    else:
-        # Find lowest price in null_prices
-        valid_null_prices = [d for d in null_prices if d["validFrom"] <= entry["validFrom"]]
-        default = None if len(valid_null_prices) == 0 else min(valid_null_prices, key=lambda x: x["unitPrice"])["unitPrice"]
-        if default is None:
-            optimized_price.append({
-                "validFrom": entry["validFrom"],
-                "validUntil": entry["validUntil"],
-                "unitPrice": entry["unitPrice"]
-            })
-            continue
-        # Add default price
-        optimized_price.append({
-            "validFrom": optimized_price[i - 1]["validUntil"],
-            "validUntil": entry["validFrom"],
-            "unitPrice": default
-        })
-        # Add entry
-        optimized_price.append({
-            "validFrom": entry["validFrom"],
-            "validUntil": entry["validUntil"],
-            "unitPrice": entry["unitPrice"]
-        })
+    # If the current price is lower than the previous price
+    elif current["unitPrice"] < previous["unitPrice"]:
+        optimized_price[-1]["validUntil"] = current["validFrom"]
+        optimized_price.append({ "validFrom": current["validFrom"], "validUntil": current["validUntil"], "unitPrice": current["unitPrice"] })
+    
+    # Handle the remainders
+    elif done:
+        optimized_price.append({ "validFrom": previous["validUntil"], "validUntil": current["validUntil"], "unitPrice": current["unitPrice"] })
+    
+    previous = current
+    if not sorted_data:
+        sorted_data = [d for d in lowest_price_data if d["validUntil"] > optimized_price[-1]["validUntil"]]
+        done = True
 
 
-# Find lowest price in null_prices
-valid_null_prices = [d for d in null_prices if d["validFrom"] <= optimized_price[-1]["validUntil"]]
-default = None if len(valid_null_prices) == 0 else min(valid_null_prices, key=lambda x: x["unitPrice"])["unitPrice"]
-# Add last default price
-if default is not None:
-    optimized_price.append({
-        "validFrom": optimized_price[-1]["validUntil"],
-        "validUntil": None,
-        "unitPrice": default
-    })
 
-
+############################################################################################################
 print()
 print("=====================================================")
 print("___________________The data output___________________")
