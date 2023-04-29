9cd2afae8c58aa3474f2f808dc8d08afee598cbe
Olof Enström <oe222fh@student.lnu.se>
2023-04-29 16:33:17 +0200

    Converted algorithm from Python to C#

--- src/Controllers/PriceController.cs
+++ src/Controllers/PriceController.cs
@@ -31,21 +31,64 @@ public class PriceController : ControllerBase {
   /// <remarks>Path: /Price/:sku</remarks>
   [HttpGet("{sku}")]
   public async Task<ActionResult<IEnumerable<Price>>> GetPrice(string sku) {
-    var prices = await _context.Price
-      .Where(p => p.CatalogEntryCode == sku && p.MarketId == "sv")
-      // Orcer by ValidUntil descending nulls are the greatest, then by ValidFrom descending
-      .OrderByDescending(p => p.ValidUntil == null)
-      .ThenByDescending(p => p.ValidUntil)
-      // .ThenByDescending(p => p.ValidFrom)
-      .ToListAsync();
-    // List<Price> result = new();
-    // for (var i = 0; i < prices.Count; i++) {
-    //   var p = new Price {
-    //     PriceValueId = prices[i].PriceValueId
-    //   };
-    // }
+    var prices = await _context.Price.Where(p => p.CatalogEntryCode == sku && p.MarketId == "sv").ToListAsync();
+    prices.ForEach(p => p.ValidUntil ??= DateTime.MaxValue);
+    var lowestPrices = prices.OrderBy(p => p.UnitPrice).ThenByDescending(p => p.ValidUntil).ToList();
+    var sortedPrices = prices.OrderBy(p => p.ValidFrom).ThenBy(p => p.ValidUntil).ThenBy(p => p.UnitPrice).ToList();
+    
+    var optimizedPrices = new List<Price> { sortedPrices.First().Clone() };
 
-    return prices;
+    var done = false;
+    var previous = sortedPrices.First();
+    sortedPrices.RemoveAt(0);
+    while (sortedPrices.Count > 0) {
+      var current = sortedPrices.First();
+      sortedPrices.RemoveAt(0);
+
+      // If there is a gap between the current price and the previous price
+      if (current.ValidFrom > previous.ValidUntil) {
+        var didNotBreak = true;
+        foreach (var entry in lowestPrices) {
+          if (entry.ValidFrom <= previous.ValidUntil && entry.ValidUntil > previous.ValidUntil) {
+            var temp = entry.Clone();
+            optimizedPrices.Add(temp);
+            previous = temp;
+            sortedPrices.Insert(0, current);
+            didNotBreak = false;
+            break;
+          }
+        }
+        if (didNotBreak) {
+         optimizedPrices.Add(current.Clone());
+         previous = current;
+        }
+        continue;
+      }
+      
+      else if (current.UnitPrice < previous.UnitPrice) {
+        optimizedPrices.Last().ValidUntil = current.ValidFrom;
+        optimizedPrices.Add(current.Clone());
+      }
+      
+      else if (done) {
+        var temp = current.Clone();
+        temp.ValidFrom = optimizedPrices.Last().ValidUntil;
+        optimizedPrices.Add(temp);
+      }
+      
+      previous = current;
+      if (sortedPrices.Count == 0) {
+        foreach (var entry in lowestPrices) {
+          if (entry.ValidUntil > optimizedPrices.Last().ValidUntil) {
+            sortedPrices.Add(entry);
+          }
+        }
+        done = true;
+      }
+    }
+
+
+    return optimizedPrices;
   }
 
   private bool PriceExists(int id) {
--- src/Models/Price.cs
+++ src/Models/Price.cs
@@ -9,7 +9,21 @@ public class Price {
   public string? CatalogEntryCode { get; set; }
   public string? MarketId { get; set; }
   public string? CurrencyCode { get; set; }
-  public DateTime ValidFrom { get; set; }
+  public DateTime? ValidFrom { get; set; }
   public DateTime? ValidUntil { get; set; }
   public decimal UnitPrice { get; set; }
+  // Set columntype to decimal(18,6) here instead of in ApplicationDbContext.cs
+  
+  public Price Clone() {
+    return new Price {
+      Created = Created,
+      Modified = Modified,
+      CatalogEntryCode = CatalogEntryCode,
+      MarketId = MarketId,
+      CurrencyCode = CurrencyCode,
+      ValidFrom = ValidFrom,
+      ValidUntil = ValidUntil,
+      UnitPrice = UnitPrice
+    };
+  }
 }
