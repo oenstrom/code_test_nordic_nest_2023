7761bb4a31f1a26719835696bd2cdd030ca944f5
Olof Enström <oe222fh@student.lnu.se>
2023-04-30 14:50:43 +0200

    Remove import of non-existing component

--- src/ClientApp/src/components/Layout.js
+++ src/ClientApp/src/components/Layout.js
@@ -1,5 +1,3 @@
-import NavMenu from './NavMenu'
-
 export default function Layout({ children }) {
   return (
     <div className="h-screen flex justify-center items-center bg-gradient-to-b from-slate-500 via-slate-400 to-slate-500">
