0c3db9ecc8e3d87ba8f43ac6777daac0567c775a
Olof Enström <oe222fh@student.lnu.se>
2023-04-30 14:43:42 +0200

    Remove comment and unused file

--- src/ClientApp/src/App.js
+++ src/ClientApp/src/App.js
@@ -15,20 +15,3 @@ export default function App() {
     </Layout>
   )
 }
-
-// export default class App extends Component {
-//   static displayName = App.name;
-//
-//   render() {
-//     return (
-//       <Layout>
-//         <Routes>
-//           {AppRoutes.map((route, index) => {
-//             const { element, ...rest } = route;
-//             return <Route key={index} {...rest} element={element} />;
-//           })}
-//         </Routes>
-//       </Layout>
-//     );
-//   }
-// }
